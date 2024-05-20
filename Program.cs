9fa03426e9d4c3dae205344098ebc2629da1e7fc
agos4567 <agosgalfre23@gmail.com>
2024-05-20 18:16:53 -0300

    calculadorav1

--- Program.cs
+++ Program.cs
@@ -3,55 +3,72 @@
 
 
 Console.WriteLine("Hello, World!");
-int a;
-int b;
-a=10;
-b=a;
-Console.WriteLine("valor de a:"+a);
-Console.WriteLine("valor de b:"+b);
+// int a;
+// int b;
+// a=10;
+// b=a;
+// Console.WriteLine("valor de a:"+a);
+// Console.WriteLine("valor de b:"+b);
 
 
-float floatNum1=0;
+int opcion;
 
-bool anda =false;
-string? numString="";
-
-while (!anda)
+do
 {
-    Console.WriteLine("Ingrese un numero valido: ");
-    numString=Console.ReadLine();
-    anda =float.TryParse(numString, out floatNum1);
-   
-    if (!anda)
+    
+
+    Console.WriteLine("Ingrese el primer numero: ");
+
+
+    int a = Convert.ToInt32(Console.ReadLine());  // convierte lo que ingreso por teclado a entero?
+
+
+      Console.WriteLine("Ingrese el segundo numero: ");
+
+
+     int b = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Elija una opcion: ");
+
+        Console.WriteLine("1/ -Sumar-  2/ -Restar  3/-Multiplicar ");
+
+     
+     opcion=Convert.ToInt32(Console.ReadLine());
+
+           switch (opcion)
     {
-        Console.WriteLine(numString + "no es un numero valido:  ");
+        case 1: 
+            Console.WriteLine(a+b);
+            break;
+        case 2: 
+            Console.WriteLine(a-b);
+            break;
+        case 3: 
+            Console.WriteLine(a*b);
+            break;
+        case 4: 
+            if(b == 0){
+                Console.WriteLine("No es posible dividir en 0.");
+            }else{
+                Console.WriteLine(a/b);
+            }
+            break;
+        default:
+
+        Console.WriteLine("Opcion invalida.");
+        Console.ReadLine();
+        break;
+
+      
     }
-    // else{
-    //     Console.WriteLine(numString +  "si es un numero valido:  ");
-    // }
-
-}
-
-
-//para invertir el numero
 
 
 
-int ReverseNumber(int number)
-{
-    int reversedNumber=0;
-  while (number !=0)
-  {
-    int digit= number %10;
-    reversedNumber=reversedNumber * 10 + digit;
-    number /=10;
-  }
+  Console.WriteLine("Desea realizar otra operacion (1.SI / 0.NO):");
+    opcion = Convert.ToInt32(Console.ReadLine());  // convierte lo que ingreso por teclado a entero lo guarda en opcion para analiza si sale del do while o sigue preguntando?
 
 
-  return reversedNumber;
-}
-int intNum= (int)floatNum1;
 
-int NumIntv=ReverseNumber(intNum);
+} while (opcion !=0);
 
-Console.WriteLine(  "Numero invertido: "  + NumIntv );
+
