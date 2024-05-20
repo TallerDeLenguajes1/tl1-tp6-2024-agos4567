fdd56a93f989950189a8cff0b00fffc402f08dc9
agos4567 <agosgalfre23@gmail.com>
2024-05-20 17:32:58 -0300

    primer punto

--- Program.cs
+++ Program.cs
@@ -9,3 +9,49 @@ a=10;
 b=a;
 Console.WriteLine("valor de a:"+a);
 Console.WriteLine("valor de b:"+b);
+
+
+float floatNum1=0;
+
+bool anda =false;
+string? numString="";
+
+while (!anda)
+{
+    Console.WriteLine("Ingrese un numero valido: ");
+    numString=Console.ReadLine();
+    anda =float.TryParse(numString, out floatNum1);
+   
+    if (!anda)
+    {
+        Console.WriteLine(numString + "no es un numero valido:  ");
+    }
+    // else{
+    //     Console.WriteLine(numString +  "si es un numero valido:  ");
+    // }
+
+}
+
+
+//para invertir el numero
+
+
+
+int ReverseNumber(int number)
+{
+    int reversedNumber=0;
+  while (number !=0)
+  {
+    int digit= number %10;
+    reversedNumber=reversedNumber * 10 + digit;
+    number /=10;
+  }
+
+
+  return reversedNumber;
+}
+int intNum= (int)floatNum1;
+
+int NumIntv=ReverseNumber(intNum);
+
+Console.WriteLine(  "Numero invertido: "  + NumIntv );
