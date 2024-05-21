0b86e0119463b9a1d34c3072f64fe293e7c68228
agos4567 <agosgalfre23@gmail.com>
2024-05-20 22:30:47 -0300

    ejercicio3

--- Program.cs
+++ Program.cs
@@ -1,57 +1,205 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
-
-
 Console.WriteLine("Hello, World!");
-int a;
-int b;
-a=10;
-b=a;
-Console.WriteLine("valor de a:"+a);
-Console.WriteLine("valor de b:"+b);
 
 
-float floatNum1=0;
+
+int opcion;
+
+
+
+int a, b;
 
 bool anda =false;
-string? numString="";
 
-while (!anda)
+
+
+
+do
 {
-    Console.WriteLine("Ingrese un numero valido: ");
-    numString=Console.ReadLine();
-    anda =float.TryParse(numString, out floatNum1);
-   
-    if (!anda)
+     
+
+
+     do
+     {
+        
+         Console.WriteLine("Ingrese el primero numero: ");
+
+            anda = int.TryParse(Console.ReadLine(), out a);// convierte lo que escribo 
+    
+          Console.WriteLine("Ingrese el segundo numero: ");
+
+            anda = int.TryParse(Console.ReadLine(), out b);
+
+         Console.WriteLine("Elija una opcion: ");
+
+        Console.WriteLine("1/ -Sumar-  2/ -Restar  3/-Multiplicar ");
+
+     
+     anda=int.TryParse(Console.ReadLine(), out opcion);
+
+
+
+     } while (!anda);
+
+
+
+
+    // Console.WriteLine("Ingrese el primero numero: ");
+
+
+    // int a = Convert.ToInt32(Console.ReadLine());  // convierte lo que ingreso por teclado a entero?
+
+
+
+    // // anda =int.TryParse(ingreso, out ingreso);
+
+    //   Console.WriteLine("Ingrese el segundo numero: ");
+
+
+    //  int b = Convert.ToInt32(Console.ReadLine());
+
+    // // anda =int.TryParse(ingreso, out ingreso);
+
+    // Console.WriteLine("Elija una opcion: ");
+
+    //     Console.WriteLine("1/ -Sumar-  2/ -Restar  3/-Multiplicar ");
+
+     
+    //  opcion=Convert.ToInt32(Console.ReadLine());
+
+           switch (opcion)
     {
-        Console.WriteLine(numString + "no es un numero valido:  ");
+        case 1: 
+            Console.WriteLine(a+b);
+            break;
+        case 2: 
+            Console.WriteLine(a-b);
+            break;
+        case 3: 
+            Console.WriteLine(a*b);
+            break;
+        case 4: 
+            if(b == 0){
+                Console.WriteLine("No es posible dividir en 0.");
+            }else{
+                Console.WriteLine(a/b);
+            }
+            break;
+        default:
+
+        Console.WriteLine("Opcion invalida.");
+        Console.ReadLine();
+        break;
+
+      
     }
-    // else{
-    //     Console.WriteLine(numString +  "si es un numero valido:  ");
-    // }
-
-}
-
-
-//para invertir el numero
 
 
 
-int ReverseNumber(int number)
-{
-    int reversedNumber=0;
-  while (number !=0)
+  Console.WriteLine("Desea realizar otra operacion (1.SI / 0.NO):");
+    opcion = Convert.ToInt32(Console.ReadLine());  // convierte lo que ingreso por teclado a entero lo guarda en opcion para analiza si sale del do while o sigue preguntando?
+
+
+
+} while (opcion !=0);
+
+
+/*calculadora 2*/
+
+   
+int x;
+int  mcm, mcd, a2, b2, minimo;
+int num1, num2;
+
+
+bool bandera =false;
+
+int a1, b1;
+
+  
+
+  while (!bandera)
   {
-    int digit= number %10;
-    reversedNumber=reversedNumber * 10 + digit;
-    number /=10;
-  }
+    
+    Console.WriteLine("Ingrese un numero: ");
+    bandera = int.TryParse(Console.ReadLine(), out x);
+    
+
+   
+
+        Console.WriteLine("Valor absoluto: "+ Math.Abs(x));
+        Console.WriteLine("Cuadrado: "+ Math.Pow(x, 2));
+        Console.WriteLine("Raiz Cuadrada: "+ Math.Sqrt(x));
+        Console.WriteLine("Seno: "+ Math.Sin(x));
+        Console.WriteLine("Coseno: "+ Math.Cos(x));
+        string astr = x.ToString();
+        string[] textSplit = astr.Split(","); // le saca la coma
+        Console.WriteLine("Parte entera: "+ textSplit[0]);
+      Console.WriteLine("Ingrese dos numeros: ");
 
 
-  return reversedNumber;
-}
-int intNum= (int)floatNum1;
+         Console.WriteLine("Primer numero: ");
 
-int NumIntv=ReverseNumber(intNum);
+  bandera = int.TryParse(Console.ReadLine(), out a1);
 
-Console.WriteLine(  "Numero invertido: "  + NumIntv );
+    // int a = Convert.ToInt32(Console.ReadLine());
+
+
+     Console.WriteLine("Segundo numero: ");
+  bandera = int.TryParse(Console.ReadLine(), out b1);
+
+//    int  b = Convert.ToInt32(Console.ReadLine());
+
+   
+    
+   mcd=0;
+
+  a2=a1;
+  b2=b1;
+
+
+   num1=Math.Max(a2,b2);
+   num2=Math.Min(a2,b2);
+
+
+     do
+     {
+        mcd=num2;
+        num2=num1%num2;
+        num1=mcd;
+     } while (num2!=0);
+
+    Console.WriteLine("El maximo es: "+ mcd);
+
+  
+    minimo=Math.Min(a1,b1);
+    mcm=0;
+
+
+    for (int i = 1; i <= minimo; i++)
+    {
+        if (a1%i==0 && b1%i==0)
+        {
+            mcd=i;
+
+        mcm=(a1*b1)/mcd;
+        
+     }
+}  
+
+  Console.WriteLine("El minimo es: "+ mcm);
+
+  Console.ReadKey(true);
+
+
+
+
+        
+ } 
+
+
+
+
+
+
+ 
